2f16abaae35304a96721b5aa95ad05ca483dc311
warrenniu <warrenniu@gmail.com>
2020-10-06 00:38:23 -0400

    Completed

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+require 'pry'
+class Dog
+    def initialize(name, breed = "Mutt")
+        # binding.pry
+        @name = name
+        @breed = breed
+    end
+    
+
+end
+# binding.pry
+
+   
+     
+     
